12fc2a00fcda7043e3bd19bbd4b56f0e77659e18
Saurabh Kumar <saurabh030198@gmail.com>
2024-09-19 16:26:29 +0530

    initial setup

---
+++ src/main/java/com/example/Vikash_Mitra/VikashMitraApplication.java
@@ -0,0 +1,13 @@
+package com.example.Vikash_Mitra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VikashMitraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VikashMitraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Vikash_Mitra/VikashMitraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Vikash_Mitra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VikashMitraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
